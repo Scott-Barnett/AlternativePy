82351f990ea8d623f89f91a00681a0c651b7aea7
Scott Barnett <sacbarnett@gmail.com>
2020-04-17 21:11:42 +0100

    Fix formatting and get absolute base directory

--- alternativepy.py
+++ alternativepy.py
@@ -11,14 +11,14 @@ from bs4 import BeautifulSoup
 PYTHON_BASE_URL = "https://www.python.org/ftp/python"
 PYTHON_SOURCE_URL = PYTHON_BASE_URL + "/{}/Python-{}.tgz"
 PYTHON_ASC_URL = "{}.asc"
-DOWNLOAD_LOCATION = "./PythonVersions/"
+BASE_DIRECTORY = os.path.abspath('.')
+DOWNLOAD_LOCATION = os.path.join(BASE_DIRECTORY, "PythonVersions")
 
 def get_confirmation(prompt: str) -> bool:
     response = None
     while response not in ['Y', 'N']:
         response = input(f"{prompt}: ").upper()
     return response == "Y"
-
 
 def get_valid_python_versions() -> list:
     html = urllib.request.urlopen(PYTHON_BASE_URL)
@@ -61,23 +61,23 @@ def download_python_version(version: str):
         exit(1)
     if not os.path.exists(DOWNLOAD_LOCATION):
         os.mkdir(DOWNLOAD_LOCATION)
-    PYTHON_DIR = os.path.join(DOWNLOAD_LOCATION, version)
-    if os.path.exists(PYTHON_DIR):
+    python_dir = os.path.join(DOWNLOAD_LOCATION, version)
+    if os.path.exists(python_dir):
         if not get_confirmation("This python version already exists - would you like to redownload it"):
             print("Aborting...")
             exit(1)
-        shutil.rmtree(PYTHON_DIR)
-    os.mkdir(PYTHON_DIR)
+        shutil.rmtree(python_dir)
+    os.mkdir(python_dir)
     python_url = PYTHON_SOURCE_URL.format(version, version)
-    python_location = f"{PYTHON_DIR}/{version}.tgz"
+    python_location = f"{python_dir}/{version}.tgz"
     urllib.request.urlretrieve(python_url, python_location)
     f = tarfile.open(python_location, mode="r:gz")
-    f.extractall(PYTHON_DIR)
+    f.extractall(python_dir)
     f.close()
-    os.chdir(os.path.join(PYTHON_DIR, f"Python-{version}"))
+    os.chdir(os.path.join(python_dir, f"Python-{version}"))
     execute_terminal_command("./configure --enable-optimizations --with-ensurepip=install")
     execute_terminal_command("make")
-    os.chdir("../../..")
+    os.chdir(BASE_DIRECTORY)
 
 if __name__ == "__main__":
     if sys.version_info < (3, 6):
